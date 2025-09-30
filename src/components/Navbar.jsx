5dfa3846c76d7ad291fba04c54fc64743233ac49
donghaxkim <donghaxkim@gmail.com>
2025-09-30 04:54:21 -0400

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,34 +6,6 @@ import { useState, useEffect } from 'react';
 
 const Navbar = ({ theme, toggleTheme }) => {
   const [activeSection, setActiveSection] = useState('');
-  const [showNavbar, setShowNavbar] = useState(false);
-  let hideTimeout;
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setShowNavbar(true);
-      clearTimeout(hideTimeout);
-      hideTimeout = setTimeout(() => setShowNavbar(false), 3000);
-    };
-
-    const handleMouseMove = (e) => {
-      if (e.clientY < 100) {
-        setShowNavbar(true);
-        clearTimeout(hideTimeout);
-      } else {
-        hideTimeout = setTimeout(() => setShowNavbar(false), 2000);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-      window.removeEventListener('mousemove', handleMouseMove);
-      clearTimeout(hideTimeout);
-    };
-  }, []);
 
   // Set active section on scroll
   useEffect(() => {
@@ -66,11 +38,7 @@ const Navbar = ({ theme, toggleTheme }) => {
   return (
     <>
       {/* TOP NAVBAR */}
-      <nav
-        className={`fixed top-4 left-0 w-full z-50 transition-opacity duration-500 ${
-          showNavbar ? 'opacity-100' : 'opacity-0 pointer-events-none'
-        }`}
-      >
+      <nav className="fixed top-4 left-0 w-full z-50">
         <div className="container mx-auto">
           <div className={`w-full ${navBg} h-[60px] backdrop-blur-3xl rounded-full max-w-[700px] mx-auto px-5 flex justify-between text-2xl ${iconColor}`}>
             <Link
@@ -124,7 +92,7 @@ const Navbar = ({ theme, toggleTheme }) => {
       <nav className="fixed top-[40%] transform -translate-y-1 right-3 z-50">
         <div className="container mx-auto">
           <div className={`${sideNavBg} w-[40px] py-4 backdrop-blur-xl rounded-full flex flex-col items-center justify-center space-y-6 z-50`}>
-            {['home', 'about', 'techstack', 'projects'].map((section) => (
+            {['home', 'about', 'techstack', 'projects', 'journey'].map((section) => (
               <Link
                 key={section}
                 to={section}
